67ef22f486066d77227135cac72b6f84a56712ef
martincukal <guschuma@student.gu.se>
2017-10-04 17:56:09 +0200

    fixed redundency and small update on email checker

--- src/Console/Console.java
+++ src/Console/Console.java
@@ -5,39 +5,46 @@ package Console;
   */
 import java.util.Scanner;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import parse.ReadJSON;
 import sendEmail.*;
+
 //Mock version for our application using the console
 public class Console {
-	
+
 	private static ReadJSON rj = new ReadJSON();
-	
-	
+	private static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		console();
 	}
 
 	public static void console() {
 		print();
-		Scanner scan = new Scanner(System.in);
 		String option = scan.nextLine();
 		chooseOption(option);
-		
-
 	}
-	public static void email(){
-		Scanner scan = new Scanner(System.in);
+
+	public static void runEmail() {
 		System.out.println("Type your email address:\n");
 		String email = scan.nextLine();
-		System.out.println("Sending the file............\n");
-		sendingAnEmail mail = new sendingAnEmail(email);
+		System.out.println("Checking if email is valid............\n");
+		if (isValidEmailAddress(email)) {
+			System.out.println("Email is valid, sending the file............\n");
+			sendingAnEmail mail = new sendingAnEmail(email);
+		} else {
+			System.out.println("\nInvalid email please try again \n");
+			runEmail();
+		}
 
 	}
-	
-	public static void chooseOption(String option){
-		Scanner scan = new Scanner(System.in);
-		
+
+	public static void chooseOption(String option) {
+
 		switch (option) {
+
 		case "1":
 			System.out.println("Parsing JSON file");
 			System.out.println("...........................................");
@@ -48,19 +55,19 @@ public class Console {
 		case "2":
 			System.out.println("Starting simulation");
 			System.out.println("...........................................");
-			//Call your function here
+			// Call your function here
 			goBack();
 			break;
 		case "3":
 			System.out.println("Recording and saving simulation");
 			System.out.println("...........................................");
-			//Call your function here
+			// Call your function here
 			goBack();
 			break;
 		case "4":
 			System.out.println("...........................................");
-			//Call your function here
-			email();
+			// Call your function here
+			runEmail();
 			goBack();
 			break;
 		case "quit":
@@ -89,15 +96,16 @@ public class Console {
 		System.out.println("Application terminated");
 		System.exit(0);
 	}
+
 	public static void back() {
 		System.out.println("\n Going back to main menu \n");
 		console();
 	}
-	
-	public static void goBack(){
+
+	public static void goBack() {
+		System.out.println("-----------------------------------------------------------");
 		System.out.println("To go back to main menu type: back");
-		System.out.println("To exit the app type: quit");
-		Scanner scan = new Scanner(System.in);
+		System.out.println("To exit the app type: quit\n");
 		String decision = scan.nextLine();
 		switch (decision) {
 		case "back":
@@ -110,7 +118,18 @@ public class Console {
 			System.out.println("\nInvalid input please try again ");
 			goBack();
 		}
-		
+
 	}
-	
+
+	public static boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}
+
 }
