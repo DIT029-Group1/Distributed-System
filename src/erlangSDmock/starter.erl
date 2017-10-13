d15cbf82b511611699368743f6b853aa8832d0da
martincukal <guschuma@student.gu.se>
2017-10-13 17:00:28 +0200

    small fix in starter

--- src/erlangSDmock/starter.erl
+++ src/erlangSDmock/starter.erl
@@ -20,10 +20,7 @@ start_sup_ch(Name)->
 	register(Name,Child),
 	{ok,Child}.
 	
-	%whereis(root_sup) ! {start_child,self(),Name},
-	%receive
-		%{start_child_reply,Pid,ChildName}  -> {ok,ChildName}
-	%end.
+	
 
 % Read through a list of messages that are sent from java script to erlang example for testing
 %	starter:read_messages([{node1,msgrequest,node2},{node2,msgrequest,node3},{node3,msgrequest,node4},{node4,msgrequest,node5},{node5,msgrequest,node6}]).
@@ -32,9 +29,13 @@ read_messages([{From,Msg,To}|Xs]) ->
 		io:format("~p -> ~p -> ~p ~n",[From,Msg,To]), timer:sleep(700), read_messages(Xs).
 
 
-server_read_messages(Server,[]) ->  no_more_msg;
-server_read_messages(Server,[{From,Msg,To}|Xs]) -> 
-		Server ! {From,Msg,To},timer:sleep(700), read_messages(Xs).
+server_read_messages(Server,[]) ->  Server ! no_more_msg;
+server_read_messages(Server,[{From,Msg,To}|Xs]) -> Server ! {{From,Msg,To},self()},timer:sleep(700),
+		receive 
+				{{From,Msg,To},Pid} -> {From,Msg,To} % call function to send it back to java script
+		end,
+   server_read_messages(Server,Xs).												 
+	
 
 
 
