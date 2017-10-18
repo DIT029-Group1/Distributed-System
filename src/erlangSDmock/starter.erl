bb57535e0adc18154a303f10e1b85edc6287962e
Marko Stanoevich <MStanoevich@gmail.com>
2017-10-18 11:43:04 +0200

    Some back-end fixes and minor refactoring

--- src/erlangSDmock/starter.erl
+++ src/erlangSDmock/starter.erl
@@ -2,7 +2,7 @@
 
 -module(starter).
 
--export([start_sup_ch/1,start_root/0,loop/1,start/0,read_messages/1,start_chsup_ch/2,spawn_actors/2,test/0,connect/0,send/2]).
+-export([start_sup_ch/1,start_root/0,read_messages/1,start_chsup_ch/2,spawn_actors/2,test/0,connect/0,send/2]).
 
 connect() ->
 	{ok,S} = gen_tcp:connect("localhost",8800,[binary,{packet,0}]),{ok,S}.
@@ -15,17 +15,17 @@ test () ->
 	% 	Actors = [a1,a2,a3,a4,a5].
 	% 	starter:spawn_actors(whereis(child2),Actors).
 	
-
+%	starter:start_root().
 start_root() ->
-	%	starter:start_root().
 	case whereis(root_sup) of 
 		undefined ->	{ok, Pid} = root_sup:start_link(),
 						{ok, Pid};
-		Pid -> {ok,Pid}
+		Pid -> 			io:format("Root supervisor is already running.~n"),
+						{ok,Pid}
 	end.	
 	
-
-%% 	starter:start_sup_ch(sd_sup).
+%% DOES NOT WORK
+% 	starter:start_sup_ch(sd_sup).
 start_sup_ch(Name)-> 
 	{ok, Child} = supervisor:start_child(whereis(root_sup), []),
 	register(Name,Child),
@@ -39,23 +39,24 @@ start_chsup_ch(Name,SupervisorPID)->
 	
 	
 
-% Read through a list of messages that are sent from java script to erlang example for testing
+%% Read through a list of messages that are sent from javascript to erlang example for testing.
+
 %	starter:read_messages([{node1,fwd,node2,msgrequest},{node2,fwd,node3,msgrequest},{node3,fwd,node4,msgrequest},{node4,fwd,node5,msgrequest},{node5,fwd,node6,msgrequest}]).
 read_messages([]) ->  no_more_msg;
 read_messages([{From,Fwd,To,Msg}|Xs]) ->
 		io:format("~p, ~p, ~p, ~p ~n",[From,Fwd,To,Msg]), timer:sleep(700), read_messages(Xs).
 
-
+%%% Unused
 server_read_messages(Server,[]) ->  Server ! no_more_msg;
 server_read_messages(Server,[{From,Msg,To}|Xs]) -> Server ! {{From,Msg,To},self()},timer:sleep(700),
 		receive 
-				{{From,Msg,To},Pid} -> {From,Msg,To} % call function to send it back to java script
+				{{From,Msg,To},_} -> {From,Msg,To} % call function to send it back to java script
 		end,
    server_read_messages(Server,Xs).		
 
-%%% Pid is the id of the child sd_simulator_OTP
-spawn_actors(Pid,[]) -> no_more_actors;
-spawn_actors(Pid,[Actor|Ys]) -> sup:start_actor(Pid),spawn_actors(Pid, Ys).
+%% Pid is the id of the child sd_simulator_OTP
+spawn_actors(_,[]) -> no_more_actors;
+spawn_actors(Pid,[_|Ys]) -> sup:start_actor(Pid),spawn_actors(Pid, Ys).
 	
 
 
--- src/erlangSDmock/sup.erl
+++ src/erlangSDmock/sup.erl
@@ -5,9 +5,10 @@
 -export([init/1]).
 -export([stop/1,start_actor/1, send_msg/4, read/1]).
 
-%Supervisor that in a way represent the client of the website and it supervises  all of the SD diagram children processes
+%% Supervisor that in a way represents the client of the website and it supervises  all of the SD diagram children processes.
 start_link() ->
-			%no name is given to this supervisor since making more than one instace with the same name will result in an error
+			%% No name is given to this supervisor since making more than one instace with the same name will result in an error.
+			%% Future TODO, do dynamic naming.
 			{ok, Pid} = supervisor:start_link(?MODULE, []),
 			io:format("Supervisor is running ~n"),
 			{ok, Pid}.
@@ -15,8 +16,8 @@ start_link() ->
    
 
 init(_Args) ->
-	%simple one for one strategy meaningn that when the supervisor is started no children are started they are all  added dinamicly 
-	% and if one child node crashes only that one is restarted
+	%% Simple one_for_one strategy meaning that when the supervisor is started no children are started they are all  added dinamically 
+	%% and if one child node crashes only that one is restarted.
      RestartStrategy = {simple_one_for_one, 10, 60},
      ChildSpec = {child, {sd_simulator_OTP, start_link, []},
           permanent, brutal_kill, worker, [sd_simulator_OTP]},
@@ -29,7 +30,6 @@ stop(Pid) ->
 read(Pid) ->
 	gen_server:call(Pid, read).
 
-%% CHANGE TO CALL: ActorFrom and ActorTo are stored as "ok" instead as their pids.
 start_actor(Pid) ->
 	gen_server:call(Pid, start_actor).
 
