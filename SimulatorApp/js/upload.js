709edac9de39c0937b7a07c49d22d830a07cf40b
martincukal <guschuma@student.gu.se>
2017-12-16 14:44:18 +0100

    Comments

--- SimulatorApp/js/upload.js
+++ SimulatorApp/js/upload.js
@@ -1,4 +1,6 @@
-﻿function UploadFile(fileUpload) {
+//author: Martin Chukaleski
+//this function is used for the simultaneous upload after choosing the file, without pressing a submit button
+function UploadFile(fileUpload) {
 	if (fileUpload.value != '') {
 		document.getElementById("<%=UploadButton.ClientID %>").click();
 	}
